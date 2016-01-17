38544ec6e17c04d1a1eae7276f746daee5ffbf24
Curt J. Sampson <cjs@cynic.net>
2016-01-17 16:22:30 +0900

    npm install --save-dev gulp-nodeunit-runner and add stub test file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const gulp = require('gulp')
+const nodeunit_runner = require('gulp-nodeunit-runner')
 
 gulp.task('test', function() {
+    gulp.src('./src/**/*.jt').pipe(nodeunit_runner())
 });
