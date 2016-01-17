f7687a95ce03fdb00d105764a295aaa1f8415c37
Curt J. Sampson <cjs@cynic.net>
2016-01-18 00:30:19 +0900

    gas-date-sort/Code.js: a bunch more stuff to play with.

--- src/gas-date-sort/Code.js
+++ src/gas-date-sort/Code.js
@@ -1,9 +1,41 @@
 'use strict'
 
+/*
+ * @customfunction
+ */
 function f() {
     Logger.log('add-on f()')
+    return 'add-on'
 }
 
+/*
+ * @customfunction
+ */
+function g() {
+    Logger.log('add-on g()')
+    return 'add-on g'
+}
+
+/*
+ * @customfunction
+ */
 function hello(name) {
     return 'Hello, ' + name
+}
+
+function setB2Hello() {
+    const ss = SpreadsheetApp.getActiveSpreadsheet()
+    ss.getSheets()[0].getRange(2, 2, 1, 1).setFormula('=hello(2)')
+}
+
+function onInstall(e) {
+    onOpen(e)
+}
+
+function onOpen(e) {
+    const ss = SpreadsheetApp.getActiveSpreadsheet()
+    ss.addMenu('Test Add-on!', [
+        { name: 'g', functionName: 'g' },
+        { name: 'setB2Hello', functionName: 'setB2Hello' }
+    ])
 }
